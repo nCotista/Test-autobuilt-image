c3efc226e954263f9c6e2451d762706fa29545da
nCotista <116904600+nCotista@users.noreply.github.com>
2025-05-31 00:46:38 +0700

    test build backend

---
+++ ฺBackend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		message := os.Getenv("APP_MESSAGE")
+		if message == "" {
+			message = "Hello from Gin inside Docker!"
+		}
+		c.JSON(http.StatusOK, gin.H{"message": message})
+	})
+
+	router.Run(":8080") // Listen and serve on 0.0.0.0:8080
+}
